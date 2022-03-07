7ef16d8d400a26f52a9258f7be079deeda2efc73
purnendu samanta <joysamanta123@gmail.com>
2022-03-07 22:49:21 +0530

    Finishing touch on layout

--- app/src/main/java/com/purnendu/studentregistration/MainActivity.kt
+++ app/src/main/java/com/purnendu/studentregistration/MainActivity.kt
@@ -3,19 +3,19 @@ package com.purnendu.studentregistration
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.AutoCompleteTextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 
 class MainActivity : AppCompatActivity() {
 
     private val department= arrayListOf("IT","CSE","Mechanical","Electrical","Electronics","MCA","MBA")
     private val year= arrayListOf("2018","2019","2020","2021","2022","2023","2024")
-    private lateinit var spinnerYear: Spinner
-    private lateinit var spinnerDepartment: Spinner
+    private lateinit var txtField1:TextInputLayout
+    private lateinit var txtField2:TextInputLayout
     private lateinit var button:MaterialButton
 
 
@@ -27,21 +27,21 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
 
-        spinnerDepartment=findViewById(R.id.permanantAddressEditText)
-        spinnerYear=findViewById(R.id.editTextName)
+        txtField1=findViewById(R.id.textInputField1)
+        txtField2=findViewById(R.id.textInputField2)
         button=findViewById(R.id.nextButtonFirstRegistration)
 
 
         val departmentSpinnerAdapter: ArrayAdapter<String> =
-            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, department)
-        departmentSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerDepartment.adapter = departmentSpinnerAdapter
+            ArrayAdapter<String>(this, R.layout.list_item, department)
+        (txtField2.editText as? AutoCompleteTextView)?.setAdapter(departmentSpinnerAdapter)
 
 
         val yearSpinnerAdapter: ArrayAdapter<String> =
-            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, year)
-        yearSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spinnerYear.adapter = yearSpinnerAdapter
+            ArrayAdapter<String>(this, R.layout.list_item, year)
+        (txtField1.editText as? AutoCompleteTextView)?.setAdapter(yearSpinnerAdapter)
+
+
 
 
         button.setOnClickListener {
